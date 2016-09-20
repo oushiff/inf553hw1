03cac155a7a65de687fcef3d701122565fd51d32
fanf <fanf@oupeng.com>
2016-09-19 17:34:15 -0700

    save

--- test.scala
+++ test.scala
@@ -12,24 +12,59 @@ import scala.util.parsing.json._
 
 
 object Sentiment {
-  def main(arg: Array[String]) {
+
+  def main(arg: Array[String]): Unit = {
+    test(Array("fei_fan_first20.txt", "AFINN-111.txt"))
+  }
 
 
-    val sparkConf = new SparkConf().setAppName("Sentiment_score")
+  def getText(line: String): Int = {
+    val words:Array[String] = line.split(" ")
+
+    val rate = 0
+
+    for ( word <- words ) {
+      if (word.toLowerCase().eq("dd")) {
+        
+      }
+      //val lowercase = word.toLowerCase()
+      //println(lowercase)
+    }
+
+    return 1
+  }
+
+
+  def test(arg: Array[String]) {
+
+
+    val sparkConf = new SparkConf().setAppName("Sentiment_score").setMaster("local[1]")
     val sc = new SparkContext(sparkConf)
 
     val textTwitter = sc.textFile(arg(0))
     val textAFINN = sc.textFile(arg(1))
 
 
-    val counts = textTwitter.foreach {
-      JSON.parseFull(_).filter( key => key == "text")
-    }
+//    val counts = textTwitter
+//      .map(line=>JSON.parseFull(line))
+
+    val counts = textTwitter
+      .flatMap(line=>JSON.parseFull(line).get.asInstanceOf[Map[String,String]].get("text"))
+      .map(line => getText(line))
+      //.map(line=>line.split(" ").toString().toLowerCase())
+
+
+    println(counts.count())
+
+
+
     //val aaa = JSON.parseFull()
 
     //val text = scala.io.Source.fromFile(arg(0), "UTF-8").toString().toLowerCase().split(" ").map(word => (word, 1))
 
-    textTwitter.saveAsTextFile("testsave.txt")
+    //counts.saveAsTextFile("testsave.txt")
+
+
 
   }
 }
